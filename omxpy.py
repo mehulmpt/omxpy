ad500bae6d1fcd9bec8c5c15c679de215bf47ae1
Mehul Mohan <mehulmp@gmail.com>
2016-10-31 01:22:25 +0530

    First commit

---
+++ omxpy.py
@@ -0,0 +1,73 @@
+import pexpect
+import re
+import os
+from threading import Thread
+import time
+
+class omxpy(object):
+    _DONE_REXP = re.compile(b"have a nice(.*?)")
+
+    _LAUNCH_CMD = '/usr/bin/omxplayer "%s" -b --key-config=keys'
+    _PAUSE_CMD = 'x'
+    _TOGGLE_SUB_CMD = 's'
+    _QUIT_CMD = 'q'
+
+    _JUMP_30SEC = 'm'
+    _FALL_30SEC = 'o'
+    _JUMP_10MIN = 'n'
+    _FALL_10MIN = 'z'
+    _REWIND_ = 'r'
+    _FORWARD_ = 'f'
+    _SUBTITLE_ = 't'
+    _DELAYSUB_ = 'd'
+    _FASTSUB_ = 'i'
+
+    subtitles_visible = True
+
+    def killchild(self):
+        while True:
+            time.sleep(3)
+            index = self._process.expect([pexpect.TIMEOUT,pexpect.EOF,self._DONE_REXP], timeout=5)
+            if index != 0:
+            	#kill scripts, etc.
+                self._process.kill(0)
+        
+    def __init__(self, filepath):
+
+        cmd = self._LAUNCH_CMD % (filepath)
+        
+        Thread(target=self.killchild, args=()).start()
+
+    def jump30s(self):
+        self._process.send(self._JUMP_30SEC)
+
+    def fall30s(self):
+        self._process.send(self._FALL_30SEC)
+
+    def jump10min(self):
+        self._process.send(self._JUMP_10MIN)
+
+    def fall10min(self):
+        self._process.send(self._FALL_10MIN)        
+
+    def toggle_pause(self):
+        self._process.send(self._PAUSE_CMD)
+
+    def toggle_subtitles(self):
+        self._process.send(self._TOGGLE_SUB_CMD):
+
+    def fastf(self):
+        self._process.send(self._FORWARD_):
+
+    def rewind(self):
+        self._process.send(self._REWIND_):
+
+    def delaysub(self):
+        self._process.send(self._DELAYSUB_):
+
+    def fastsub(self):
+        self._process.send(self._FASTSUB_):
+
+    def stop(self):
+        self._process.send(self._QUIT_CMD)
+        self._process.terminate(force=True)
